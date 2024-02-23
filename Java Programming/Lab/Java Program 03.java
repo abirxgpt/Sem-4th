77c86dc67633712fe5c7a544378ae24b06b28de8
Abir Gupta <142162873+abirxgpt@users.noreply.github.com>
2024-02-23 10:38:10 +0530

    Update Java Program 03.java

--- Java Programming/Lab/Java Program 03.java
+++ Java Programming/Lab/Java Program 03.java
@@ -14,18 +14,13 @@ public class Main
 	if (num2 > max)
 	  {
 		max = num2;
-		System.out.print ("Maximum of these numbers is numbers 2 = " + num2);
 
 	  }
 	if (num3 > max)
 	  {
 		max = num3;
-		System.out.print ("Maximum of these numbers is numbers 3 = " + num3);
 
 	  }
-	else
-	  {
-		System.out.print ("Maximum of these numbers is numbers 1 = " + num1);
-	  }
+	System.out.print ("Maximum of these numbers is: " + max);
   }
 }
